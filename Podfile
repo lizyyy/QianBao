304620975b5c85ca480d78a221464d92de6b6675
leeey <heyleezy@gmail.com>
2017-04-19 15:54:06 +0800

    添加pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+target "QianBao" do
+pod 'FMDB', '~>2.6.2'
+end
---
+++ Pods/Target Support Files/Pods-QianBao/Pods-QianBao-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_QianBaoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QianBaoVersionString[];
+
